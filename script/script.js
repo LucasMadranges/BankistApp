b68d57842cac0abc325e9bd2de0ba5ec7489eef3
Lucas Madranges <lucas.madranges.pro@icloud.com>
2023-11-03 11:51:12 +0100

    1.0.23 : Add current date for label date

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
 'use strict';
 
 createUsername(accounts);
+
+const currentDate = new Date();
+const currentDay = `${currentDate.getDay()}`.padStart(2, '0');
+const currentMonth = `${currentDate.getMonth() + 1}`.padStart(2, '0');
+const currentYear = currentDate.getFullYear();
+const currentHours = currentDate.getHours();
+const currentMinutes = currentDate.getMinutes();
+labelDate.textContent = `${currentDay}/${currentMonth}/${currentYear}, ${currentHours}:${currentMinutes}`;
