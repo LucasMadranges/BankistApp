a726c54d31d272750cdc1d4239e0d2df9581b76f
Lucas Madranges <lucas.madranges.pro@icloud.com>
2023-11-04 09:29:58 +0100

    1.0.37 : Delete useless script.js

--- script/script.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-createUsername(accounts);
--- script/variables.js
+++ script/variables.js
@@ -69,3 +69,5 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+
+createUsername(accounts);
